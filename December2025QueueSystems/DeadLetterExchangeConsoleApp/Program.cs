7ff683945d767e449693dd7c0b6015c3ea423bc8
Burak Ibrahim Unal <32199163+Burak-Ibrahim-Unal@users.noreply.github.com>
2026-01-12 09:10:26 +0300

    mainQueueArguments

--- December2025QueueSystems/DeadLetterExchangeConsoleApp/Program.cs
+++ December2025QueueSystems/DeadLetterExchangeConsoleApp/Program.cs
@@ -2,117 +2,252 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 
-Console.WriteLine("Dead Letter Exchange");
+(IConnection connection, IChannel channel) = await DeadLetterScenario1();
 
-const string mainExchange = "main.exchange";
-const string mainQueue = "main.queue";
-
-const string deadLetterExchange = "dead.letter.exchange";
-const string deadLetterQueue = "dead.letter.queue";
-
-var connectionFactory = new ConnectionFactory
+async Task<(IConnection connection, IChannel channel)> DeadLetterScenario1()
 {
-    Uri = new Uri("amqps://oypfspzz:jcWNYrihOkCeXeWy02f_oYhmeyO2MRBm@leopard.lmq.cloudamqp.com/oypfspzz")
-};
+    Console.WriteLine("Dead Letter Exchange");
 
-using var connection = await connectionFactory.CreateConnectionAsync();
-using var channel = await connection.CreateChannelAsync();
+    const string mainExchange = "main.exchange";
+    const string mainQueue = "main.queue";
 
-//main
-await channel.ExchangeDeclareAsync(
-    exchange: mainExchange,
-    type: ExchangeType.Fanout,
-    durable: true, // restart durumunda kalıcı olacak
-    autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
-    arguments: null
-);
+    const string deadLetterExchange = "dead.letter.exchange";
+    const string deadLetterQueue = "dead.letter.queue";
 
-// Dead letter exchange ayarları QueueDeclareAsync'te arguments içinde belirtilmelidir.
-var mainQueueArguments = new Dictionary<string, object>()
-{
-    {"x-dead-letter-exchange", deadLetterExchange}, // Dead letter exchange adını belirtir.
-    {"x-message-ttl", 10000 } // Mesajların 10 saniye sonra expire olmasını sağlar.
-};
+    var connectionFactory = new ConnectionFactory
+    {
+        Uri = new Uri("amqps://oypfspzz:jcWNYrihOkCeXeWy02f_oYhmeyO2MRBm@leopard.lmq.cloudamqp.com/oypfspzz")
+    };
 
-await channel.QueueDeclareAsync(
-    queue: mainQueue,
-    durable: true, // Kuyruk kalıcı olacak
-    exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
-    autoDelete: false, // Kuyruk otomatik silinmeyecek
-    arguments: mainQueueArguments // Dead letter exchange ayarları burada belirtilir.
-);
+    using var connection = await connectionFactory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
 
-await channel.QueueBindAsync(
-    queue: mainQueue,
-    exchange: mainExchange,
-    routingKey: string.Empty,
-    arguments: null // QueueBindAsync'te dead letter ayarları belirtilmez.
-);
+    //main
+    await channel.ExchangeDeclareAsync(
+        exchange: mainExchange,
+        type: ExchangeType.Fanout,
+        durable: true, // restart durumunda kalıcı olacak
+        autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
+        arguments: null
+    );
 
-// dead letter
-await channel.ExchangeDeclareAsync(
-    exchange: deadLetterExchange,
-    type: ExchangeType.Fanout,
-    durable: true, // restart durumunda kalıcı olacak
-    autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
-    arguments: null
-);
+    // Dead letter exchange ayarları QueueDeclareAsync'te arguments içinde belirtilmelidir.
+    var mainQueueArguments = new Dictionary<string, object>()
+    {
+        {"x-queue-type", "quorum"}, // Kuyruk tipleri
+        // "classic" : Eski tip kuyruktur, RAM/disk ağırlıklıdır, çoğu gelişmiş retry/delivery-limit özelliği yoktur.
+        // "quorum"  : Raft tabanlı, veri kaybına dayanıklı, retry & delivery-limit destekleyen, güvenilir kuyruk tipidir.
+        // "stream"  : Çok yüksek throughput ve event streaming için tasarlanmıştır, log gibi çalışır (Kafka benzeri).Mesajlar asla silinmez.
+        {"x-dead-letter-exchange", deadLetterExchange}, // Dead letter exchange adını belirtir.
+        {"x-message-ttl", 10000 }, // Mesajların 10 saniye sonra expire olmasını sağlar.
+        {"x-delivery-limit",3 } // Bir mesaj en fazla 3 kez yeniden kuyruğa teslim edilmeye çalışılır; 3 denemeden sonra başarısız kabul edilip dead-letter exchange’e yönlendirilir.
+    };
 
-// Dead letter queue için arguments ayarları
-var deadLetterQueueArguments = new Dictionary<string, object>()
+    await channel.QueueDeclareAsync(
+        queue: mainQueue,
+        durable: true, // Kuyruk kalıcı olacak
+        exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
+        autoDelete: false, // Kuyruk otomatik silinmeyecek
+        arguments: mainQueueArguments // Dead letter exchange ayarları burada belirtilir.
+    );
+
+    await channel.QueueBindAsync(
+        queue: mainQueue,
+        exchange: mainExchange,
+        routingKey: string.Empty,
+        arguments: null // QueueBindAsync'te dead letter ayarları belirtilmez.
+    );
+
+    // dead letter
+    await channel.ExchangeDeclareAsync(
+        exchange: deadLetterExchange,
+        type: ExchangeType.Fanout,
+        durable: true, // restart durumunda kalıcı olacak
+        autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
+        arguments: null
+    );
+
+    // Dead letter queue için arguments ayarları
+    var deadLetterQueueArguments = new Dictionary<string, object>()
 {
     {"x-dead-letter-exchange", deadLetterExchange }, // Dead letter queue'daki mesajların 24 saat (86400000 ms) sonra expire olmasını sağlar.
     {"x-message-ttl", 20000 } // Dead letter queue'da maksimum 10000 mesaj tutulur, sonrasında eski mesajlar silinir.
 };
 
-await channel.QueueDeclareAsync(
-    queue: deadLetterQueue,
-    durable: true, // Kuyruk kalıcı olacak
-    exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
-    autoDelete: false, // Kuyruk otomatik silinmeyecek
-    arguments: deadLetterQueueArguments // Dead letter queue arguments ayarları
-);
+    await channel.QueueDeclareAsync(
+        queue: deadLetterQueue,
+        durable: true, // Kuyruk kalıcı olacak
+        exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
+        autoDelete: false, // Kuyruk otomatik silinmeyecek
+        arguments: deadLetterQueueArguments // Dead letter queue arguments ayarları
+    );
 
-await channel.QueueBindAsync(
-    queue: deadLetterQueue,
-    exchange: deadLetterExchange,
-    routingKey: string.Empty,
-    arguments: null
-);
+    await channel.QueueBindAsync(
+        queue: deadLetterQueue,
+        exchange: deadLetterExchange,
+        routingKey: string.Empty,
+        arguments: null
+    );
 
 
-// send messages to main.exchange
-var messageBody = "Bir bir test verisidir.";
+    // send messages to main.exchange
+    var messageBody = "Bir bir test verisidir.";
 
-var properties = new BasicProperties()
+    var properties = new BasicProperties()
+    {
+        Persistent = true,
+        Expiration = "10000"
+    };
+
+    var body = System.Text.Encoding.UTF8.GetBytes(messageBody);
+    await channel.BasicPublishAsync(
+        exchange: mainExchange,
+        routingKey: string.Empty,
+        body: body
+    );
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+
+    consumer.ReceivedAsync += async (sender, eventArgs) =>
+    {
+        try
+        {
+            var receivingMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            Console.WriteLine($"Received message from dead letter queue: {receivingMessage} ");
+            await channel.BasicAckAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+        }
+        catch (Exception)
+        {
+            await channel.BasicNackAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false, requeue: false);
+            throw;
+        }
+
+    };
+
+    await channel.BasicConsumeAsync(queue: deadLetterQueue, autoAck: false, consumer: consumer);
+    return (connection, channel);
+}
+
+async Task<(IConnection connection, IChannel channel)> DeadLetterScenario2WithRequeue()
 {
-    Persistent = true,
-    Expiration = "10000"
+    Console.WriteLine("Dead Letter Exchange");
+
+    const string mainExchange = "main.exchange";
+    const string mainQueue = "main.queue";
+
+    const string deadLetterExchange = "dead.letter.exchange";
+    const string deadLetterQueue = "dead.letter.queue";
+
+    var connectionFactory = new ConnectionFactory
+    {
+        Uri = new Uri("amqps://oypfspzz:jcWNYrihOkCeXeWy02f_oYhmeyO2MRBm@leopard.lmq.cloudamqp.com/oypfspzz")
+    };
+
+    using var connection = await connectionFactory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    //main
+    await channel.ExchangeDeclareAsync(
+        exchange: mainExchange,
+        type: ExchangeType.Fanout,
+        durable: true, // restart durumunda kalıcı olacak
+        autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
+        arguments: null
+    );
+
+    // Dead letter exchange ayarları QueueDeclareAsync'te arguments içinde belirtilmelidir.
+    var mainQueueArguments = new Dictionary<string, object>()
+{
+    {"x-dead-letter-exchange", deadLetterExchange}, // Dead letter exchange adını belirtir.
+    {"x-message-ttl", 10000 } // Mesajların 10 saniye sonra expire olmasını sağlar.
 };
 
-var body = System.Text.Encoding.UTF8.GetBytes(messageBody);
-await channel.BasicPublishAsync(
-    exchange: mainExchange,
-    routingKey: string.Empty,
-    body: body
-);
+    await channel.QueueDeclareAsync(
+        queue: mainQueue,
+        durable: true, // Kuyruk kalıcı olacak
+        exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
+        autoDelete: false, // Kuyruk otomatik silinmeyecek
+        arguments: mainQueueArguments // Dead letter exchange ayarları burada belirtilir.
+    );
 
-var consumer = new AsyncEventingBasicConsumer(channel);
+    await channel.QueueBindAsync(
+        queue: mainQueue,
+        exchange: mainExchange,
+        routingKey: string.Empty,
+        arguments: null // QueueBindAsync'te dead letter ayarları belirtilmez.
+    );
 
-consumer.ReceivedAsync += async (sender, eventArgs) =>
+    // dead letter
+    await channel.ExchangeDeclareAsync(
+        exchange: deadLetterExchange,
+        type: ExchangeType.Fanout,
+        durable: true, // restart durumunda kalıcı olacak
+        autoDelete: false, // Bu exhange bağlı kuyruk olmadığında silinmesin
+        arguments: null
+    );
+
+    // Dead letter queue için arguments ayarları
+    var deadLetterQueueArguments = new Dictionary<string, object>()
 {
-    try
-    {
-        var receivingMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
-        Console.WriteLine($"Received message from dead letter queue: {receivingMessage} ");
-        await channel.BasicAckAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false);
-    }
-    catch (Exception)
-    {
-        await channel.BasicNackAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false, requeue: false);
-        throw;
-    }
-
+    {"x-dead-letter-exchange", deadLetterExchange }, // Dead letter queue'daki mesajların 24 saat (86400000 ms) sonra expire olmasını sağlar.
+    {"x-message-ttl", 20000 } // Dead letter queue'da maksimum 10000 mesaj tutulur, sonrasında eski mesajlar silinir.
 };
 
-await channel.BasicConsumeAsync(queue: deadLetterQueue, autoAck: false, consumer: consumer);
+    await channel.QueueDeclareAsync(
+        queue: deadLetterQueue,
+        durable: true, // Kuyruk kalıcı olacak
+        exclusive: false, // Kuyruk sadece mevcut bağlantıya özel değil, bağlantı kapansa bile kalacak
+        autoDelete: false, // Kuyruk otomatik silinmeyecek
+        arguments: deadLetterQueueArguments // Dead letter queue arguments ayarları
+    );
+
+    await channel.QueueBindAsync(
+        queue: deadLetterQueue,
+        exchange: deadLetterExchange,
+        routingKey: string.Empty,
+        arguments: null
+    );
+
+
+    // send messages to main.exchange
+    var messageBody = "Bir bir test verisidir.";
+
+    var properties = new BasicProperties()
+    {
+        Persistent = true,
+        Expiration = "10000"
+    };
+
+    var body = System.Text.Encoding.UTF8.GetBytes(messageBody);
+    await channel.BasicPublishAsync(
+        exchange: mainExchange,
+        routingKey: string.Empty,
+        body: body
+    );
+
+    var consumer = new AsyncEventingBasicConsumer(channel);
+
+    consumer.ReceivedAsync += async (sender, eventArgs) =>
+    {
+        try
+        {
+            var receivingMessage = System.Text.Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+            Console.WriteLine($"Received message from dead letter queue: {receivingMessage} ");
+            await channel.BasicAckAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false);
+        }
+        catch (Exception)
+        {
+            await channel.BasicNackAsync(deliveryTag: eventArgs.DeliveryTag, multiple: false, requeue: false);
+            throw;
+        }
+
+    };
+
+    await channel.BasicConsumeAsync(queue: deadLetterQueue, autoAck: false, consumer: consumer);
+    return (connection, channel);
+}
+
+
+
+
+
